573ad88f199e4526005660ea9397270b6ec537ac
Lucas Diego Petris <lucas.petris@outlook.com>
2021-09-15 21:54:57 -0300

    Atualizando...

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function soma(num1, num2) {
+	return num1 + num2;
+}
+
+function subtracao(num1, num2) {
+	return num1 - num2;
+}
+
+function multiplicao(num1, num2) {
+	return num1 * num2;
+}
+
+function divisao(num1, num2) {
+	return num1 / num2;
+}
+
+exports.multiplicao = multiplicao;
+exports.divisao = divisao;
+exports.soma = soma;
+exports.subtracao = subtracao;
